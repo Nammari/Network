a26adfda957d979463ddd8d6ed25a597700c2100
Nammari <nammariahmad@gmail.com>
2016-03-28 19:19:16 +0200

    fix gridspansize

--- library/src/main/java/nammari/network/ui/EndlessMultiStateAbsListFragment.java
+++ library/src/main/java/nammari/network/ui/EndlessMultiStateAbsListFragment.java
@@ -550,7 +550,7 @@ public abstract class EndlessMultiStateAbsListFragment<T> extends
         return new GridLayoutManager.SpanSizeLookup() {
             @Override
             public int getSpanSize(int position) {
-                return adapter.getItemViewType(position) == NetworkWrapperAdapter.VIEW_TYPE_LOADING ? 1 : getGridSpan();
+                return (adapter.getItemViewType(position) == NetworkWrapperAdapter.VIEW_TYPE_LOADING) ? getGridSpan() : 1;
             }
         };
     }
